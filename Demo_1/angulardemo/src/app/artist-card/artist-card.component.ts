ec10b65173c5e43fdb458074eec31052c727b4ae
jevans40 <jevans40@uwyo.edu>
2021-04-16 09:16:42 -0600

    Implemented lambda functions for DB

--- Demo_1/angulardemo/src/app/artist-card/artist-card.component.ts
+++ Demo_1/angulardemo/src/app/artist-card/artist-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-artist-card',
   templateUrl: './artist-card.component.html',
   styleUrls: ['./artist-card.component.css']
 })
+
 export class ArtistCardComponent implements OnInit {
 
   constructor() { }
